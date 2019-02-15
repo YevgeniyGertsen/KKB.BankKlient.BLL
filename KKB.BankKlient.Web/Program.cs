1b90dbc7e8f108ddfd6484e6a510a8c00081673c
zhenya <zhenya@AYAKOVLEV>
2019-02-15 10:53:31 +0600

    Add project files.

---
+++ KKB.BankKlient.Web/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KKB.BankKlient.BLL.User;
+using KKB.BankKlient.BLL.User.Account;
+
+namespace KKB.BankKlient.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            ServiceUser service = new ServiceUser();
+
+            User user = new User();
+            user.FirstName = "Евгений";
+            user.LastName = "Герцен";            
+            user.Login = "admin";
+            user.Password = "admin";
+
+            string message = "";
+            if (service.RegisterUser(user, out message))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
